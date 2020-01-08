446873ef3a68a73f6a2dd92a83aca98cbb5f0fb9
Ha Do <>
2020-01-08 15:44:41 +0700

    Fix tab spacing on tablet

--- SignKeys.Controls/SignKeys.Controls.Droid/TopTabbedPageRenderer.cs
+++ SignKeys.Controls/SignKeys.Controls.Droid/TopTabbedPageRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using Android.Content;
 using Android.Support.Design.Widget;
@@ -54,6 +55,7 @@ namespace SignKeys.Controls.Droid
 
         protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
+            calculatedWidth = 0;
             base.OnElementPropertyChanged(sender, e);
             if (TopTabLayout == null || !(Element is TopTabbedPage page)) return;
             if (e.PropertyName == TopTabbedPage.ShadowedTabbarProperty.PropertyName)
@@ -76,36 +78,122 @@ namespace SignKeys.Controls.Droid
                 TopTabLayout.TabIndicatorFullWidth = page.IsHighlighterFullWidth;
             }
         }
-
-        void UpdateTabDistribution(bool equalWidth)
+        int calculatedWidth = 0;
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
         {
+            base.OnLayout(changed, l, t, r, b);
+            if (Device.Idiom == TargetIdiom.Tablet && null != TopTabLayout)
+            {
+                UpdateTabsWidthOnTablet(r - l);
+            }
+        }
+
+        void UpdateTabsWidthOnTablet(int containerWidth)
+        {
+            if (containerWidth == calculatedWidth) return;
+            calculatedWidth = containerWidth;
             var slidingTabStrip = (ViewGroup)TopTabLayout.GetChildAt(0);
             var count = TopTabLayout.TabCount;
             var i = 0;
-            if (equalWidth)
+            float totalWidth = 0;
+            var widths = new float[count];
+            while (i < count)
             {
-                while (i < count)
+                var padding = (float)Context.FromPixels(40);
+
+                var tab = slidingTabStrip.GetChildAt(i);
+                if (tab is TabLayout.TabView tv)
                 {
-                    var tab = slidingTabStrip.GetChildAt(i);
-                    i++;
-                    var layoutParams = tab.LayoutParameters as LinearLayout.LayoutParams;
-                    layoutParams.Weight = 1;
-                    layoutParams.Width = LinearLayout.LayoutParams.WrapContent;
-                    tab.LayoutParameters = layoutParams;
+                    if (tv.ChildCount >= 2 && tv.GetChildAt(1) is TextView textView)
+                    {
+                        var w = (float)Context.ToPixels(Measure(textView)) + padding;
+                        totalWidth += w;
+                        widths[i] = w;
+                    }
+                    else
+                    {
+                        var w = (float)Context.FromPixels(Measure(tv.Tab.Text)) + padding;
+                        totalWidth += w;
+                        widths[i] = w;
+                    }
                 }
+                i++;
+            }
+            i = 0;
+            float ratio = 1;
+            if (containerWidth > totalWidth)
+            {
+                ratio = (float)containerWidth / (float)totalWidth;
+            }
+            while (i < count)
+            {
+                var tab = slidingTabStrip.GetChildAt(i);
+                var layoutParams = tab.LayoutParameters as LinearLayout.LayoutParams;
+                layoutParams.Weight = 1;
+                layoutParams.Width = (int)Math.Ceiling(widths[i] * ratio);
+                tab.LayoutParameters = layoutParams;
+                i++;
+            }
+        }
+
+        void UpdateTabDistribution(bool equalWidth)
+        {
+            if (Device.Idiom == TargetIdiom.Tablet)
+            {
+                UpdateTabsWidthOnTablet(Width);
             }
             else
             {
-                while (i < count)
+                var slidingTabStrip = (ViewGroup)TopTabLayout.GetChildAt(0);
+                var count = TopTabLayout.TabCount;
+                var i = 0;
+                TopTabLayout.TabMode = TabLayout.ModeScrollable;
+                if (equalWidth)
                 {
-                    var tab = slidingTabStrip.GetChildAt(i);
-                    i++;
-                    var layoutParams = tab.LayoutParameters as LinearLayout.LayoutParams;
-                    layoutParams.Weight = 1;
-                    layoutParams.Width = LinearLayout.LayoutParams.WrapContent;
-                    tab.LayoutParameters = layoutParams;
+                    while (i < count)
+                    {
+                        var tab = slidingTabStrip.GetChildAt(i);
+                        i++;
+                        var layoutParams = tab.LayoutParameters as LinearLayout.LayoutParams;
+                        layoutParams.Weight = 1;
+                        layoutParams.Width = LinearLayout.LayoutParams.WrapContent;
+                        tab.LayoutParameters = layoutParams;
+                    }
+                }
+                else
+                {
+                    while (i < count)
+                    {
+                        var tab = slidingTabStrip.GetChildAt(i);
+                        i++;
+                        var layoutParams = tab.LayoutParameters as LinearLayout.LayoutParams;
+                        layoutParams.Weight = 1;
+                        layoutParams.Width = LinearLayout.LayoutParams.WrapContent;
+                        tab.LayoutParameters = layoutParams;
+                    }
                 }
             }
         }
+
+        public static int Measure(TextView textView)
+        {
+            var text = textView.Text;
+            if (string.IsNullOrEmpty(text)) return 0;
+            var bounds = new Android.Graphics.Rect();
+            var textPaint = textView.Paint;
+            textPaint.GetTextBounds(text, 0, text.Length, bounds);
+            int height = bounds.Height();
+            int width = bounds.Width();
+            return width;
+        }
+
+        public int Measure(string text)
+        {
+            return Measure(new TextView(this.Context)
+            {
+                Text = text
+            });
+        }
     }
+
 }
